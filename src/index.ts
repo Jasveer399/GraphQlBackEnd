83f75812e8a9b7c875deb9effc600eba2ffda1fb
jassijasit <jasveersingh182002@gmail.com>
2024-07-11 18:05:20 +0530

    Setup graphql Server

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+
+async function init() {
+  const app = express();
+
+  app.use(express.json());
+  const PORT = Number(process.env.PORT) || 8000;
+
+  const server = new ApolloServer({
+    typeDefs: `
+      type Query {
+        hello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        hello: () => "Hello, World!",
+      },
+    },
+  });
+
+  await server.start();
+
+  app.get("/", (req, res) => {
+    res.json({ msg: "Welcome to the API" });
+  });
+
+  app.use("/graphql", expressMiddleware(server));
+
+  app.listen(PORT, () => console.log(`Server listening on port:${PORT}`));
+}
+
+init();
